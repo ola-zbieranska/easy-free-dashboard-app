793edf2c5c19c246f6d25763e0b188512ef31a86
Aleksandra <aleksandra.zbieranska@gmail.com>
2024-04-28 14:32:41 +0200

    new code

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ import dash_bootstrap_components as dbc
 # Inicjalizacja aplikacji
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
-server = app.server  # Umożliwia wdrożenie na serwerach
+server = app.server
