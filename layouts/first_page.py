1d680403b855061d5389c131c9bef09d01b937da
Aleksandra <aleksandra.zbieranska@gmail.com>
2024-05-04 22:59:42 +0200

    new

--- layouts/first_page.py
+++ layouts/first_page.py
@@ -2,14 +2,14 @@ import dash_bootstrap_components as dbc
 from dash import html, dcc
 from components.navbar import get_navbar
 
-def get_firstpage():
-    firstpage = dbc.Card(
-    dbc.CardBody(
-        [
-            html.H5("Card title", className="card-title"),
-            html.P("This card has some text content, but not much else"),
-            dbc.Button("Go somewhere", color="primary"),
-        ]
-    )
-    )
-    return firstpage
+# def get_firstpage():
+#     firstpage = dbc.Card(
+#     dbc.CardBody(
+#         [
+#             html.H5("Card title", className="card-title"),
+#             html.P("This card has some text content, but not much else"),
+#             dbc.Button("Go somewhere", color="primary"),
+#         ]
+#     )
+#     )
+#     return firstpage
