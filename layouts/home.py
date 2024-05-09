22b49f386e8da6e7d1699cea125e980bc29b5678
Aleksandra <aleksandra.zbieranska@gmail.com>
2024-05-09 23:54:03 +0200

    new

--- layouts/home.py
+++ layouts/home.py
@@ -43,3 +43,6 @@ def get_layout():
         ])
     ], fluid=True)
     return layout
+
+
+#wprowadzić zmiany w laypucie, dodać dbc.card albo rozbić na główną stronę i dodatkowe pod czynności związane z danymi wybiaralne przez użytkownika
