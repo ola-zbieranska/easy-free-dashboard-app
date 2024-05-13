6095bf1db9dd799c078848042fa13c30a9246da1
Aleksandra <aleksandra.zbieranska@gmail.com>
2024-05-13 14:08:39 +0200

    new changes

--- layouts/documentation_page.py
+++ layouts/documentation_page.py
@@ -4,8 +4,8 @@ from dash import html
 def get_documentation_page():
     sidebar = dbc.Offcanvas(
         [
-            html.H2("Documentation", className="display-4"),
-            html.Hr(),
+            #html.H2("Documentation", className="display-4"),
+            #html.Hr(),
             html.P("Useful links for documentation:", className="lead"),
             dbc.Nav(
                 [
@@ -28,7 +28,7 @@ def get_documentation_page():
     # Główna treść strony "Documentation"
     content = html.Div([
             html.H1("Learn more about creating custom charts and dashboard in an easy & free way."),
-            html.P("Here you can find extensive documentation on how to use the dashboard and create your charts."),
+            html.P("Here you can find extensive documentation on how to use the Easy Dashboard and create your charts."),
             dbc.Button("Learn more", id="open-sidebar", n_clicks=0)
         ], style={"margin-left": "270px"})  # Przesunięcie treści, aby uwzględnić sidebar
 
--- layouts/first_page.py
+++ layouts/first_page.py
@@ -6,7 +6,7 @@ def get_first_page():
         dbc.Row(
             dbc.Col(html.Div([
                 html.H1("Create custom charts and dashboard in an easy & free way."),
-                html.P("If you need help learn how to create your first chart step by step."),
+                html.P("If you need help learn how to create your first chart step by step click below to check out tutorials and documentation."),
                 dbc.Button("Learn more", color="primary", href="/documentation", className="me-1")
             ]))
         )
