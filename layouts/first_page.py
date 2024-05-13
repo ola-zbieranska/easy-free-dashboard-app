36fba8eb8e54f21160b7cb149962475368096bac
Aleksandra <aleksandra.zbieranska@gmail.com>
2024-05-13 11:00:29 +0200

    new changes

--- layouts/first_page.py
+++ layouts/first_page.py
@@ -6,6 +6,11 @@ def get_first_page():
         dbc.Row(
             dbc.Col(html.Div([
                 html.H1("Create custom charts and dashboard in an easy & free way."),
-                html.P("Easy Dashboard takes your data from spreadsheets to shareable graphics — no coding necessary. It’s free & no sign-up is required.")
-            ], style={"marginLeft": "16rem"})))])
+                html.P("Easy Dashboard takes your data from spreadsheets to shareable graphics — no coding necessary. It’s free & no sign-up is required."),
+                dbc.Button("Create", color="primary", className="me-1"),
+                html.P("Check out tutorials and tips on how to create custom charts and dashboard."),
+                dbc.Button("Tutorial", color="primary", className="me-2"),
+            ]))
+        )
+    ])
     return first_page
