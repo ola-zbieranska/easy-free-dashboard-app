fc400617b07cf1965c9e15fc40c71b409a081d01
Aleksandra <aleksandra.zbieranska@gmail.com>
2024-05-14 21:44:28 +0200

    add upload file window

--- layouts/upload_data_page.py
+++ layouts/upload_data_page.py
@@ -7,7 +7,26 @@ def get_upload_data_page():
             dbc.Col(html.Div([
                 html.H1("Upload CSV or Excel spreadsheets."),
                 html.P("In the pop-up window, select the Excel/CSV file from your computer or drag and drop the file in the window below."),
-                html.P("If you just want to try Easy Dashboard, here‘s a list of some example datasets you can use:")
+                html.P("If you just want to try Easy Dashboard, here‘s a list of some example datasets you can use:"),
+                dcc.Upload(
+                    id='upload-data',
+                    children=html.Div([
+                        'Drag and Drop or ',
+                        html.A('Select Files')
+                    ]),
+                    style={
+                        'width': '100%',
+                        'height': '60px',
+                        'lineHeight': '60px',
+                        'borderWidth': '1px',
+                        'borderStyle': 'dashed',
+                        'borderRadius': '5px',
+                        'textAlign': 'center',
+                        'margin': '10px 0'
+                    },
+                    multiple=False  # Umożliwia wgrywanie tylko jednego pliku na raz
+                ),
+                html.Div(id='output-data-upload')
             ]))
         )
     ])
