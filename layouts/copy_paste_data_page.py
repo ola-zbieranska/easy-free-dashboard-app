cd64ba085d2927c9745b71cc4e96f50f8b4f28eb
Aleksandra <aleksandra.zbieranska@gmail.com>
2024-05-14 21:35:13 +0200

    add text field copy&paste window

--- layouts/copy_paste_data_page.py
+++ layouts/copy_paste_data_page.py
@@ -1,5 +1,6 @@
 import dash_bootstrap_components as dbc
 from dash import html, dcc
+from dash import dash_table
 
 def get_copy_paste_data_page():
     copy_paste_data_page = dbc.Container([
@@ -7,7 +8,13 @@ def get_copy_paste_data_page():
             dbc.Col(html.Div([
                 html.H1("Copy & paste your data"),
                 html.P("Select your data (including header row/column) in Excel or LibreOffice and paste it in the text field. You can also upload a CSV or Excel file from your computer."),
-                html.P("If you just want to try Easy Dashboard, here‘s a list of some example datasets you can use:")
+                html.P("If you just want to try Easy Dashboard, here‘s a list of some example datasets you can use:"),
+                dcc.Textarea(
+                    id='data-input',
+                    placeholder="Paste your copied data here...",
+                    style={'width': '100%', 'height': 200},
+                ),
+                dbc.Button("Proceed", id="proceed-to-check", color="primary", className="mt-2")
             ]))
         )
     ])
