05b5efcc967f309f9984f2f12af0f4cdc76b01cc
Aleksandra <aleksandra.zbieranska@gmail.com>
2024-05-15 13:07:21 +0200

    new callbacks, change in layouts - tabs in first page

--- callbacks/callbacks.py
+++ callbacks/callbacks.py
@@ -1,18 +1,11 @@
 import dash
-import pandas as pd
-import base64
-import io
-# import gspread
-from dash import html, dcc, Input, Output, State, dash_table
+from dash import html, Input, Output, State
 import dash_bootstrap_components as dbc
 from app import app
-#from callbacks.validation import validate_data, get_google_sheet_data, parse_upload
 from layouts.home import get_home_page as home_page
 from layouts.first_page import get_first_page as first_page
+from layouts.first_page import get_copy_paste_data, get_upload_data, get_import_data
 from layouts.documentation_page import get_documentation_page as documentation_page
-from layouts.copy_paste_data_page import get_copy_paste_data_page as copy_paste_data_page
-from layouts.upload_data_page import get_upload_data_page as upload_data_page
-from layouts.import_data_page import get_import_data_page as import_data_page
 from layouts.check_describe_page import get_check_and_describe_page as check_and_describe_page
 
 
@@ -54,111 +47,19 @@ def display_pages(pathname):
         #return get_blog_page()
     elif pathname == '/documentation':
         return documentation_page()
-    elif pathname == '/copy-paste-data-page':
-        return copy_paste_data_page()
-    elif pathname == '/upload-data-page':
-        return upload_data_page()
-    elif pathname == '/import-data-page':
-        return import_data_page()
     elif pathname == '/check-and-describe-page':
         return check_and_describe_page()
     else:
         return '404'
 
-# sidebar = offcanvas
-@app.callback(
-    Output("offcanvas", "is_open"),
-    [Input("open-sidebar", "n_clicks")],
-    [State("offcanvas", "is_open")]
-)
-def toggle_offcanvas(n, is_open):
-    if n:
-        return not is_open
-    return is_open
 
-# # Callback do nawigacji między stronami
-# @app.callback(
-#     Output('page-content', 'children'),
-#     [Input('url', 'pathname')]
-# )
-# def display_page(pathname):
-#     if pathname == '/check-data':
-#         return check_and_describe_page()
-#     elif pathname == '/import-data':
-#         return import_data_page()
-#     elif pathname == '/upload-data':
-#         return upload_data_page()
-#     elif pathname == '/copy-paste-data':
-#         return copy_paste_data_page()
-#     else:
-#         return copy_paste_data_page()
-
-# # Callback do przetwarzania danych z wklejania
-# @app.callback(
-#     Output('url', 'pathname'),
-#     Input('proceed-to-check', 'n_clicks'),
-#     State('data-input', 'value')
-# )
-# def process_copy_paste_data(n_clicks, data):
-#     if n_clicks is not None and data:
-#         df = pd.read_csv(pd.compat.StringIO(data), sep='\t', header=0)
-#         errors = validate_data(df)
-#         if not errors:
-#             return '/check-data'
-#     return dash.no_update
-#
-# # Callback do przetwarzania danych z uploadu
-# @app.callback(
-#     Output('output-data-upload', 'children'),
-#     Input('upload-data', 'contents'),
-#     State('upload-data', 'filename')
-# )
-# def process_upload_data(contents, filename):
-#     if contents is not None:
-#         content_type, content_string = contents.split(',')
-#         decoded = base64.b64decode(content_string)
-#         try:
-#             if 'csv' in filename:
-#                 df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
-#             elif 'xls' in filename:
-#                 df = pd.read_excel(io.BytesIO(decoded))
-#             errors = validate_data(df)
-#             if not errors:
-#                 return html.Div([
-#                     html.H5("Upload successful!"),
-#                     dash_table.DataTable(
-#                         id='data-table',
-#                         data=df.to_dict('records'),
-#                         columns=[{'name': i, 'id': i} for i in df.columns],
-#                         editable=True,
-#                         row_deletable=True,
-#                         page_size=10
-#                     )
-#                 ])
-#         except Exception as e:
-#             print(e)
-#     return html.Div()
-
-# # Callback do przetwarzania danych z Google Sheets
-# @app.callback(
-#     Output('output-url', 'children'),
-#     Input('import-button', 'n_clicks'),
-#     State('google-sheet-url', 'value')
-# )
-# def import_google_sheet(n_clicks, url):
-#     if n_clicks is not None and url:
-#         df = get_google_sheet_data(url)
-#         errors = validate_data(df)
-#         if not errors:
-#             return html.Div([
-#                 html.H5("Google Sheet Data:"),
-#                 dash_table.DataTable(
-#                     id='data-table',
-#                     data=df.to_dict('records'),
-#                     columns=[{'name': i, 'id': i} for i in df.columns],
-#                     editable=True,
-#                     row_deletable=True,
-#                     page_size=10
-#                 )
-#             ])
-#     return html.Div()
+# callback do obsługi dcc.Tabs na first page
+@app.callback(Output('tabs-content-example', 'children'),
+              [Input('tabs-example', 'value')])
+def render_content(tab):
+    if tab == 'tab-1':
+        return get_copy_paste_data()
+    elif tab == 'tab-2':
+        return get_upload_data()
+    elif tab == 'tab-3':
+        return get_import_data()
--- layouts/first_page.py
+++ layouts/first_page.py
@@ -1,50 +1,8 @@
 import dash_bootstrap_components as dbc
 from dash import html, dcc
+from dash.dependencies import Input, Output
+import dash
 
-# Definicja trzech różnych kart
-card1 = dbc.Card(
-    [
-        #dbc.CardHeader("Copy & paste data table"),
-        dbc.CardBody(
-            [
-                html.H4("Copy & paste your data", className="card-title"),
-                #html.P("Select your data in Excel or LibreOffice and paste it in the text field", className="card-text"),
-            ]
-        ),
-        dbc.CardFooter(dbc.Button("Copy & Paste", color="primary",href="/copy-paste-data-page")),
-    ],
-    style={"width": "18rem"},
-)
-
-card2 = dbc.Card(
-    [
-        #dbc.CardHeader("CSV/Excel upload"),
-        dbc.CardBody(
-            [
-                html.H4("Upload CSV or Excel spreadsheets", className="card-title"),
-                #html.P("Upload a CSV or Excel file from your computer", className="card-text"),
-            ]
-        ),
-        dbc.CardFooter(dbc.Button("Choose File", color="primary", href="/upload-data-page")),
-    ],
-    style={"width": "18rem"},
-)
-
-card3 = dbc.Card(
-    [
-        #dbc.CardHeader("Connect Google Sheet"),
-        dbc.CardBody(
-            [
-                html.H4("Connect Google Sheet", className="card-title"),
-                #html.P("Make sure to enable Link Sharing in the Google Sheet and copy the spreadsheet URL into the text field on the right", className="card-text"),
-            ]
-        ),
-        dbc.CardFooter(dbc.Button("Connect Sheet", color="primary", href="/import-data-page")),
-    ],
-    style={"width": "18rem"},
-)
-
-# Funkcja generująca pierwszą stronę
 def get_first_page():
     first_page = dbc.Container([
         dbc.Row(
@@ -56,13 +14,71 @@ def get_first_page():
             ]))
         ),
         dbc.Row(
-            dbc.Col(
-                dbc.CardGroup(
-                    [card1, card2, card3]
-                ),
-                width=12
-            ),
-            className="mt-4"  # margines górny
+            dcc.Tabs(id="tabs-example", value='tab-1', children=[
+                dcc.Tab(label='Copy & paste data table', value='tab-1'),
+                dcc.Tab(label='XLS/CSV upload', value='tab-2'),
+                dcc.Tab(label='Connect Google Sheet', value='tab-3'),
+            ]),
+            className="mt-4"
         ),
+        html.Div(id='tabs-content-example')
     ])
     return first_page
+
+def get_copy_paste_data():
+    return html.Div([
+        html.H1("Copy & paste your data"),
+        html.P("Select your data (including header row/column) in Excel or LibreOffice and paste it in the text field. You can also upload a CSV or Excel file from your computer."),
+        html.P("If you just want to try Easy Dashboard, here‘s a list of some example datasets you can use:"),
+        dcc.Textarea(
+            id='data-input',
+            placeholder="Paste your copied data here...",
+            style={'width': '100%', 'height': 200},
+        ),
+        dbc.Button("Check data", id="proceed-to-check", color="primary", className="mt-2", href="/check-and-describe-page")
+    ])
+
+def get_upload_data():
+    return html.Div([
+        html.H1("Upload CSV or Excel spreadsheets."),
+        html.P("In the pop-up window, select the Excel/CSV file from your computer or drag and drop the file in the window below."),
+        html.P("If you just want to try Easy Dashboard, here‘s a list of some example datasets you can use:"),
+        dcc.Upload(
+            id='upload-data',
+            children=html.Div([
+                'Drag and Drop or ',
+                html.A('Select Files')
+            ]),
+            style={
+                'width': '100%',
+                'height': '60px',
+                'lineHeight': '60px',
+                'borderWidth': '1px',
+                'borderStyle': 'dashed',
+                'borderRadius': '5px',
+                'textAlign': 'center',
+                'margin': '10px 0'
+            },
+            multiple=False  # Umożliwia wgrywanie tylko jednego pliku na raz
+        ),
+        dbc.Button("Check data", id="proceed-to-check", color="primary", className="mt-2", href="/check-and-describe-page"),
+        html.Div(id='output-data-upload')
+    ])
+
+def get_import_data():
+    return html.Div([
+        html.H1("Import data from Google Sheet."),
+        html.P([
+            "Make sure to enable ",
+            html.A("Link Sharing", href="https://support.google.com/drive/answer/2494822?visit_id=1-636596603923192043-2988837136&p=link_sharing_on&hl=en&rd=1#link_sharing&zippy=%2Callow-general-access-to-the-file", target="_blank"),
+            " in the Google Sheet and copy the spreadsheet URL into the text field below."
+        ]),
+        dcc.Input(
+            id='google-sheet-url',
+            type='url',
+            placeholder='Enter Google Sheet URL',
+            style={'width': '100%', 'margin-top': '10px'}
+        ),
+        dbc.Button("Check data", id="proceed-to-check", color="primary", className="mt-2", href="/check-and-describe-page"),
+        html.Div(id='output-url')
+    ])
