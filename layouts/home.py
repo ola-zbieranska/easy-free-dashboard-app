8d9b024566f7a061a30083a94bf02bf66d84c94c
Aleksandra <aleksandra.zbieranska@gmail.com>
2024-05-16 23:19:30 +0200

    welcome text on home page

--- layouts/home.py
+++ layouts/home.py
@@ -4,8 +4,12 @@ def get_home_page():
     layout = dbc.Container([
         dbc.Row(
             dbc.Col(html.Div([
+                html.H1("Welcom to Easy Dashboard!"),
                 html.H1("Create custom charts and dashboard in an easy & free way."),
                 html.P("Easy Dashboard takes your data from spreadsheets to shareable graphics — no coding necessary. It’s free & no sign-up is required!"),
+                html.P("Create stunning charts & maps"),
+                html.P("Upload data from three different ways"),
+                html.P("No software to install"),
                 dbc.Button("Create", color="primary", href="/first-page", className="me-1"),
                 html.P("Check out tutorials and tips on how to create custom charts and dashboard."),
                 dbc.Button("Tutorials", color="primary", href="/documentation", className="me-2"),
