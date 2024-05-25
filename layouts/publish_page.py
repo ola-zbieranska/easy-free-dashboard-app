afbc3428ece71a6986e3e71498fea2500c6bd696
Aleksandra <aleksandra.zbieranska@gmail.com>
2024-05-25 23:04:25 +0200

    new

--- layouts/publish_page.py
+++ layouts/publish_page.py
@@ -6,7 +6,7 @@ def get_publish_page():
             dbc.Col(html.Div([
                 html.H1("Publish visualization"),
                 html.H1("Embed your chart or download it as PNG."),
-                dbc.Button("Publish", color="primary", href="/first-page", className="me-1")
+                dbc.Button("Publish", color="primary", href="/", className="me-1")
             ]))
         )
     ])
